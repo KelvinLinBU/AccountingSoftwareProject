f1ccc78b5d50bd9a7f0d747c6db28e5483cf0754
Kelvin Lin <kelvin@bu.edu>
2023-02-21 14:43:00 -0500

    Add files via upload

--- AccountingSoftwareStart.py
+++ AccountingSoftwareStart.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 from balancesheet import BalanceSheet
 from account import Account
+from retainedearnings import RetainedEarnings
 
 def display_menu_software():
     """prints the options for the accounting software"""
     print()
     print('(0) Quit the program')
     print('(1) Start the balance sheet software')
-    
+    print('(2) Start the statement of retained earnings software')
     
 def display_menu_balancesheet():
     """prints the options for the balance sheet software"""
@@ -17,10 +18,6 @@ def display_menu_balancesheet():
     print('(1) Add/Modify an Account')
     print('(2) Remove an Account')
     print('(3) List of valid accounts')
-    
-    
-    
-    
     
 def AccountingSoftware():
     """The function that starts the accounting software project"""
@@ -32,6 +29,8 @@ def AccountingSoftware():
             break
         if choice == 1:
             BalancesheetStart()
+        if choice == 2:
+            RetainedEarningsStart()
         else: 
             print('Invalid Choice! Try Again')
             
@@ -60,6 +59,20 @@ def BalancesheetStart():
             balancesheet.print_account_options()
         else:
             print('Invalid Choice! Try Again')
+            
+def RetainedEarningsStart():
+    """The function that starts the statement of retained earnings project"""
+    company_name = str(input('Enter your company name: '))
+    date = str(input('Enter the date: '))
+    beginning_retained_earnings = int(input('Enter your beginning retained earnings: '))
+    net_income = int(input('Enter your net income/loss: '))
+    dividends_declared = int(input('Enter your dividends declared: '))
+    statement = RetainedEarnings(company_name, date, beginning_retained_earnings, net_income, dividends_declared)
+    print()
+    print(statement)
+    print()
+    
+   
             
             
             
